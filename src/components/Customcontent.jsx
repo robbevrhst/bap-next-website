c2acd51b87a27a987b811a5bf4eb36af61b7ee57
remcovb <remcovanbogaert@icloud.com>
2020-02-12 14:16:43 +0100

    correct data is loaded everywhere except img not found

--- src/components/Customcontent.jsx
+++ src/components/Customcontent.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './Customcontent.module.css';
 import customimg from './assets/eventimg.png';
+
+import eventimg1 from './assets/eventimg1.png';
+import eventimg2 from './assets/eventimg2.png';
+import eventimg3 from './assets/eventimg3.png';
 import bustrack from './assets/bustrack.svg';
 import bus from './assets/bus.svg';
 import firebase from '../firebase.js';
@@ -22,63 +26,57 @@ const Customcontent = items => {
   return (
     <div>
       <h1 className={styles.welcomtitle}>
-        Rico, hier is wat je samen kan beleven
+        {custom[0].name}, hier is wat je samen kan beleven {custom[0].opponent}
       </h1>
 
       <div className={styles.artistcontainer}>
-        <h3 className={styles.production}>bad translation</h3>
-        <h2 className={styles.artist}>Cris Blanco</h2>
+        <h3 className={styles.production}>{custom[0].production}</h3>
+        <h2 className={styles.artist}>{custom[0].title}</h2>
 
-        <p className={styles.description}>
-          Bad Translation is een gevecht waarbij het analoge wint van het
-          digitale. Cris Blanco ziet de digitale wereld als een grote metafoor
-          voor de samenleving. Daarin wordt ons ‘analoge’ leven steeds vaker
-          omgezet in een ‘digitale’ taal die via een computer gedeeld wordt met
-          de wereld.
-        </p>
+        <p className={styles.description}>{custom[0].description}</p>
       </div>
 
-      <img className={styles.eventimg} src={customimg} alt="" />
+      <img className={styles.eventimg} src={eventimg1} alt="" />
       <div className={styles.buscontainer}>
         <div className={styles.buttoncontainer}>
           <a className={styles.buybutton} href="">
             Koop ticket - €8
           </a>
           <h4 className={styles.freebustitle}>
-            gratis bus richting cris blanco
+            gratis bus richting {custom[0].title}
           </h4>
           <div className={styles.bustrackcontainer}>
             <p className={styles.leavetext}>
-              Vertrek aan de
-              <br /> <span className={styles.bold}>Budafabriek</span>
+              Vertrek aan
+              <br /> <span className={styles.bold}>{custom[0].vertrek}</span>
             </p>
             <div>
               <img className={styles.bustrack} src={bustrack} alt="" />
               <div className={styles.bustrackercontainer}>
-                <span className={styles.time}>14:15</span>
-                <span className={styles.time}>14:15</span>
+                <span className={styles.time}>{custom[0].bustimeleave1}</span>
+                <span className={styles.time}>{custom[0].bustimearrive1}</span>
               </div>
             </div>
             <p className={styles.arrivetext}>
-              Vertrek aan de
-              <br /> <span className={styles.bold}>Budafabriek</span>
+              Vertrek aan
+              <br /> <span className={styles.bold}>{custom[0].aankomst}</span>
             </p>
           </div>
           <div className={styles.bustrackcontainer}>
             <p className={styles.leavetext}>
-              Vertrek aan de
-              <br /> <span className={styles.bold}>Budafabriek</span>
+              Vertrek aan
+              <br /> <span className={styles.bold}>{custom[0].aankomst}</span>
             </p>
             <div>
               <img className={styles.bustrack} src={bustrack} alt="" />
               <div className={styles.bustrackercontainer}>
-                <span className={styles.time}>14:15</span>
-                <span className={styles.time}>14:15</span>
+                <span className={styles.time}>{custom[0].bustimeleave2}</span>
+                <span className={styles.time}>{custom[0].bustimearrive2}</span>
               </div>
             </div>
             <p className={styles.arrivetext}>
-              Vertrek aan de
-              <br /> <span className={styles.bold}>Budafabriek</span>
+              Vertrek aan
+              <br /> <span className={styles.bold}>{custom[0].vertrek}</span>
             </p>
           </div>
         </div>
